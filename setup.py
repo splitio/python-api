f8a0af91f5865eb0dc13faad2bfdc00a69f63a05
Martin Redolatti <martinredolatti@gmail.com>
2017-06-08 15:08:55 -0300

    fix setup.py & integrate with e2e tests

--- setup.py
+++ setup.py
@@ -1,9 +1,85 @@
 #!/usr/bin/env python
 
+import subprocess
+import contextlib
+import sys
+import pkg_resources
 from setuptools import setup, find_packages
 from os import path
+from distutils.cmd import Command
 from sys import version_info
 
+
+@contextlib.contextmanager
+def _save_argv(repl=None):
+    saved = sys.argv[:]
+    if repl is not None:
+        sys.argv[:] = repl
+    try:
+        yield saved
+    finally:
+        sys.argv[:] = saved
+
+
+class E2ETests(Command):
+    '''
+    Custom command to run End to End tests with locally mocked
+    tornado webserver.
+    '''
+    description = 'Run end to end tests with mocked tornado backend'
+    user_options = [
+        ('addopts=', None, "Additional options to be passed verbatim to the "
+         'pytest runner')
+    ]
+
+    def initialize_options(self):
+        '''
+        '''
+        self.addopts = []
+
+    def finalize_options(self):
+        '''
+        '''
+        pass
+
+    def _setup_tornado(self):
+        '''
+        '''
+        try:
+            import tornado
+        except ImportError:
+            subprocess.check_call(['pip', 'install'] + locale2e_requires)
+
+    def _setup_pytest(self):
+        '''
+        '''
+        current_dir = path.dirname(path.realpath(__file__))
+        egg_path = path.join(current_dir, '.eggs')
+        pkg_resources.working_set.add_entry(egg_path)
+        pkg_resources.require('pytest')
+
+    def _setup_args(self):
+        '''
+        '''
+        le2e_test_file = path.join(
+            path.dirname(path.realpath(__file__)),
+            'identify',
+            'tests',
+            'e2e',
+            'e2etests.py'
+        )
+        return ['pytest'] + [le2e_test_file] + self.addopts
+
+    def run(self):
+        '''
+        '''
+        self._setup_tornado()
+        self._setup_pytest()
+        import pytest
+        with _save_argv(self._setup_args()):
+            __import__('pytest').main()
+
+# Project requirements
 install_requires = [
     'argparse>=1.2.1',
     'colorlog>=2.10.0',
@@ -12,12 +88,24 @@ install_requires = [
     'wsgiref>=0.1.2',
 ]
 
-tests_requires = ['pytest', 'pytest-mock', 'tornado']
 
+# Standard unit test requirements
+tests_requires = [
+    'pytest-mock',
+    'pytest',
+]
+
+
+# Local End to End test requirements
+locale2e_requires = [
+    'tornado'
+]
+
+
+# Setup execution
 with open(path.join(path.abspath(path.dirname(__file__)),
                     'identify', 'version.py')) as f:
     exec(f.read())
-
 setup(
     name='identify_client',
     version=__version__,  # noqa
@@ -31,9 +119,7 @@ setup(
     license='Apache License 2.0',
     install_requires=install_requires,
     setup_requires=['pytest-runner'],
-    extras_requires= {
-        'tests': tests_requires,
-    },
+    tests_require=tests_requires,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -43,5 +129,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries'
     ],
-    packages=find_packages()
+    packages=find_packages(),
+    cmdclass={
+        'e2etests': E2ETests,
+    }
 )
